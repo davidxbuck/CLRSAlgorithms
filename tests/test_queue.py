a151c38394ec2794bf5190d3abed1473b50016d9
David Buck <david.buck@datascience.fun>
2019-06-17 19:50:56 +0100

    Queue renamed to Queue_ to avoid object conflict

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,10 +1,10 @@
 import pytest
 
-import Queue
+import Queue_
 
 
 def test_empty():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     assert queue.Q == [None] * 20
     assert queue.head == 0
     assert queue.tail == 0
@@ -12,7 +12,7 @@ def test_empty():
 
 
 def test_enqueue():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     queue.ENQUEUE(5)
     assert queue.Q == [5] + [None] * 19
     assert queue.head == 0
@@ -21,7 +21,7 @@ def test_enqueue():
 
 
 def test_enqueue2():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     queue.ENQUEUE(5)
     queue.ENQUEUE(6)
     assert queue.Q == [5, 6] + [None] * 18
@@ -31,7 +31,7 @@ def test_enqueue2():
 
 
 def test_enqueue19():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     for i in range(0, 19):
         queue.ENQUEUE(i)
     assert queue.Q == list(range(0, 19)) + [None]
@@ -42,13 +42,13 @@ def test_enqueue19():
 
 def test_enqueue20():
     with pytest.raises(OverflowError):
-        queue = Queue.Queue()
+        queue = Queue_.Queue()
         for i in range(0, 20):
             queue.ENQUEUE(i)
 
 
 def test_enqueuedequeue():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     queue.ENQUEUE(5)
     deq = queue.DEQUEUE()
     assert queue.Q == [5] + [None] * 19
@@ -59,15 +59,15 @@ def test_enqueuedequeue():
 
 
 def test_enqueuedequeue2():
-    with pytest.raises(Queue.Underflow):
-        queue = Queue.Queue()
+    with pytest.raises(Queue_.Underflow):
+        queue = Queue_.Queue()
         queue.ENQUEUE(5)
         deq = queue.DEQUEUE()
         deq = queue.DEQUEUE()
 
 
 def test_enqueuedequeue19():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     for i in range(0, 19):
         queue.ENQUEUE(i)
         deq = queue.DEQUEUE()
@@ -79,7 +79,7 @@ def test_enqueuedequeue19():
 
 
 def test_enqueuedequeue20():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     for i in range(0, 20):
         queue.ENQUEUE(i)
         deq = queue.DEQUEUE()
@@ -91,7 +91,7 @@ def test_enqueuedequeue20():
 
 
 def test_enqueuedequeue19b():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     for i in range(0, 19):
         queue.ENQUEUE(i)
     for i in range(0, 19):
@@ -104,7 +104,7 @@ def test_enqueuedequeue19b():
 
 
 def test_enqueuedequeue21():
-    queue = Queue.Queue()
+    queue = Queue_.Queue()
     for i in range(0, 21):
         queue.ENQUEUE(i)
         deq = queue.DEQUEUE()
